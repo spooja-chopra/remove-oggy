a4bbddad13f6dcf25a52d27256cf4b88ea78af9f
spooja-chopra <69622617+spooja-chopra@users.noreply.github.com>
2020-08-13 20:15:44 +0530

    Update Main.java

--- remover/Main.java
+++ remover/Main.java
@@ -5,6 +5,15 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void removeOggy(ArrayList<String> names) {
+         String subString = "oggy";
+			for(int i=names.size()-1; i>=0; i--)
+			{
+			   if(names.get(i).contains(subString))
+			   {
+			
+			       names.remove(i);
+			   }
+			}
         return;
     }
     public static boolean oggyIsRemoved(ArrayList<String> names) {
